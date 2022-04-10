b2c383c8f50b748087889ddb4ba942863b288822
agnishwar123 <86349916+agnishwar123@users.noreply.github.com>
2022-04-10 12:06:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+prediction = "";
+
 Webcam.set({
     width: 350,
     height: 280,
@@ -17,9 +19,55 @@ function take_snapshot()
 
 console.log('ml5 version:', ml5.version);
 
-classifier = ml5.imageClassifier('', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/toqR6GDzD/model.json', modelLoaded);
 
 function modelLoaded()
 {
     console.log("modelLoaded");
+}
+
+function speak()
+{
+    var synth = window.speechSynthesis;
+    speak_data = "The first Prediction is" + prediction ;
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utterThis);
+}
+
+function check()
+{
+    img = document.getElementById("capture_image");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results)
+{
+ if (error){
+     console.error(error);
+ }
+ else
+   {
+     console.log(results);
+     document.getElementById("result_gesture_name").innerHTML = results[0].label;
+
+     prediction = results[0].label;
+
+     speak();
+
+
+     if (results[0].label == "AMAZING")
+     {
+         document.getElementById("gesture").innerHTML = "hi";
+     }
+
+     if (results[0].label == "BEST")
+     {
+         document.getElementById("gesture").innerHTML = "hello";
+     }
+
+     if (results[0].label == "VICTORY")
+     {
+         document.getElementById("gesture").innerHTML = "Yup!";
+     }
+    }
 }
